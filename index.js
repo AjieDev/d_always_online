44296ea9dea35a8b4837a67a79dfb6f289098dd3
AjieDev <daji3874@gmail.com>
2024-01-10 08:22:42 +0700

    Add new files

---
+++ index.js
@@ -0,0 +1,12 @@
+const Eris = require("eris");
+const keep_alive = require('./keep_alive.js')
+
+// Replace TOKEN with your bot account's token
+const bot = new Eris(process.env.token);
+
+client.on("error", (err) => {
+  console.error(err); // or your preferred logger
+});
+
+
+client.connect(); // Get the bot to connect to Discord
